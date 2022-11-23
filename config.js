e02ca41b0c81368e79c6a4e951384a6989ef47b5
Aarush-DAS <78961766+Aarush-DAS@users.noreply.github.com>
2022-11-23 18:01:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD0RPrPVjhICtIoKLKD8QU7dYqKBfArxTc",
+    authDomain: "storytellingapp-1f9da.firebaseapp.com",
+    projectId: "storytellingapp-1f9da",
+    storageBucket: "storytellingapp-1f9da.appspot.com",
+    messagingSenderId: "145492920140",
+    databaseurl:"https://storytellingapp-1f9da-default-rtdb.firebaseio.com/",
+    appId: "1:145492920140:web:e8dafdd6c276a65e69f1e9"
+  };
